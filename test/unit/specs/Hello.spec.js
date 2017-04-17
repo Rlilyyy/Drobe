cd595e5fcc0f94ea6c3707b65db29318ca0e445b
Rlilyyy <bingohlee@gmail.com>
2017-04-17 18:03:31 +0800

    unit test

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,13 +1,118 @@
 import Vue from 'vue'
-import Hello from 'src/components/Hello'
+import VueRouter from 'vue-router'
+import Home from 'src/components/index/Home'
+import About from 'src/components/index/About'
+import AllArticle from 'src/components/index/AllArticle'
+import FullArticle from 'src/components/index/FullArticle'
+import TagView from 'src/components/index/TagView'
+import ChangeArticle from 'src/components/back/ChangeArticle'
+import CreateArticle from 'src/components/back/CreateArticle'
+import Login from 'src/components/back/Login'
+import ViewArticles from 'src/components/back/ViewArticles'
 
-describe('Hello.vue', () => {
-  it('should render correct contents', () => {
+const routes = [
+  { path: '/', component: Home, name: 'index' },
+  { path: '/articles/:tag', component: Home, name: 'articlesWithTag' },
+  { path: '/fullArticle/:id', component: FullArticle, name: 'fullArticle' },
+  { path: '/allArticle', component: AllArticle, name: 'allArticle' },
+  { path: '/about', component: About, name: 'about' },
+  { path: '/tagView', component: TagView, name: 'tagView' },
+
+  { path: '/back/login', component: Login, name: 'login' },
+  { path: '/back', redirect: '/back/viewArticles' },
+  { path: '/back/viewArticles', component: ViewArticles, name: 'viewArticles' },
+  { path: '/back/createArticle', component: CreateArticle, name: 'createArticle' },
+  { path: '/back/changeArticle/:id', component: ChangeArticle, name: 'changeArticle' }
+]
+
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
+
+Vue.use(VueRouter)
+
+describe('App.vue', () => {
+  it('should have component About', () => {
     const vm = new Vue({
       el: document.createElement('div'),
-      render: (h) => h(Hello)
+      render: (h) => h(About)
     })
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component AllArticle', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(AllArticle)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component FullArticle', () => {
+    const vm = new Vue({
+      router,
+      el: document.createElement('div'),
+      render: (h) => h(FullArticle)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component Home', () => {
+    const vm = new Vue({
+      router,
+      el: document.createElement('div'),
+      render: (h) => h(Home)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component TagView', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(TagView)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component ChangeArticle', () => {
+    const vm = new Vue({
+      router,
+      el: document.createElement('div'),
+      render: (h) => h(ChangeArticle)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component CreateArticle', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(CreateArticle)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component Login', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(Login)
+    })
+
+    expect(vm).to.not.be.null;
+  })
+
+  it('should have component ViewArticles', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(ViewArticles)
+    })
+
+    expect(vm).to.not.be.null;
   })
 })
